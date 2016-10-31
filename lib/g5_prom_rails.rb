be26eab4ee28c8b54fe9a0348c15efdeb0a8efac
Don Petersen <don@donpetersen.net>
2016-10-31 02:52:25 +0000

    Fix autoloading issue.
    Only shows up in implementing apps, of course.

--- lib/g5_prom_rails.rb
+++ lib/g5_prom_rails.rb
@@ -1,5 +1,3 @@
-require "g5_prom_rails/engine"
-
 module G5PromRails
   PER_PROCESS_PATH = "/metrics"
   PER_APPLICATION_PATH = "/probe"
@@ -24,3 +22,8 @@ module G5PromRails
     end
   end
 end
+
+# Down here due to fun behavior in implementing apps where G5PromRails module
+# isn't defined if a class is defined straight up as G5PromRails::Whatever.
+# Just scripting things.
+require "g5_prom_rails/engine"
